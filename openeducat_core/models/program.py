ae8f3a0842d2087d89c2383df3f3e893479b09fb
Roshni Pujara <roshni@openeducat.com>
2025-03-19 15:42:39 +0530

    [ADD] Added program object in core.

--- openeducat_core/models/program.py
+++ openeducat_core/models/program.py
@@ -23,19 +23,13 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
 
-class OpCourse(models.Model):
-    _name = "op.course"
+class OpProgram(models.Model):
+    _name = "op.program"
     _inherit = "mail.thread"
-    _description = "OpenEduCat Course"
+    _description = "OpenEduCat Program"
 
-    name = fields.Char('Name', required=True, translate=True)
-    code = fields.Char('Code', size=16, required=True)
-    parent_id = fields.Many2one('op.course', 'Parent Course')
-    evaluation_type = fields.Selection(
-        [('normal', 'Normal'), ('GPA', 'GPA'),
-         ('CWA', 'CWA'), ('CCE', 'CCE')],
-        'Evaluation Type', default="normal", required=True)
-    subject_ids = fields.Many2many('op.subject', string='Subject(s)')
+    name = fields.Char('Name', required=True, translate=True, tracking=True)
+    code = fields.Char('Code', size=16, required=True, translate=True)
     max_unit_load = fields.Float("Maximum Unit Load")
     min_unit_load = fields.Float("Minimum Unit Load")
     department_id = fields.Many2one(
@@ -43,20 +37,3 @@ class OpCourse(models.Model):
         default=lambda self:
         self.env.user.dept_id and self.env.user.dept_id.id or False)
     active = fields.Boolean(default=True)
-    program_id = fields.Many2one('op.program', string="Program", tracking=True)
-
-    _sql_constraints = [
-        ('unique_course_code',
-         'unique(code)', 'Code should be unique per course!')]
-
-    @api.constrains('parent_id')
-    def _check_category_recursion(self):
-        if self._has_cycle():
-            raise ValidationError(_('You cannot create recursive categories.'))
-
-    @api.model
-    def get_import_templates(self):
-        return [{
-            'label': _('Import Template for Courses'),
-            'template': '/openeducat_core/static/xls/op_course.xls'
-        }]
