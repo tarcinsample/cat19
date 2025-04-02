022d21ea336aa9406834e90cd4be77c6df5eac67
aky0221 <akshay@openeducat.com>
2025-04-02 12:21:06 +0200

    [18.0][Add] added openeducat theme

---
+++ theme_web_openeducat/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'OpenEduCat Demo Theme',
+    'description': 'OpenEduCat Demo Theme',
+    'category': 'Theme',
+    'version': '18.0.1.0',
+    'author': 'OpenEduCat',
+    'depends': [
+        'website',
+        'theme_default',
+    ],
+    'data': [
+        'views/homepage.xml',
+        'views/web.xml',
+    ],
+    'assets': {
+        'web._assets_primary_variables': [
+            '/theme_web_openeducat/static/src/scss/primary_variables.scss',
+        ],
+        'web.assets_frontend': [
+            '/theme_web_openeducat/static/src/scss/style.scss',
+            '/theme_web_openeducat/static/src/js/home.js',
+        ],
+    },
+    'license': 'LGPL-3',
+    'application': True,
+    'installable': True,
+}
+##############################################################################
